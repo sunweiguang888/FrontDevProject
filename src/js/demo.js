d639398d5c6ae4abe3775b68273cbe0e75c1d2cc
Sun WeiGuang(新闻客户端) <weiguangsun@sohu-inc.com>
2016-04-12 19:15:10 +0800

    立即推送

---
+++ src/js/demo.js
@@ -0,0 +1,11 @@
+function show(){
+	var wefwefwefwe = 11;
+	console.log('show方法已执行');
+	document.write('show方法已执行');//wefwef
+	console.info(1111111);
+}
+
+//window.onload = function(){
+
+	show();
+//}
